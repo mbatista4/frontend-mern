51b9d8b228f5a751e24d95c31ebd0bb8992797b8
mbatista4 <manuelbatista04@gmail.com>
2020-07-14 02:27:26 -0400

    finished basic home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,20 +1,37 @@
 import React, { useContext, useEffect } from 'react'
 import { useHistory } from 'react-router-dom';
 import UserContext from '../../context/UserContext';
+import { mainContent } from '../../css/home.module.css';
+
+
 
 export default function Home() {
 
-    const { userData } = useContext(UserContext);
+    const { userData, setUserData } = useContext(UserContext);
+
+
+
     const history = useHistory();
 
+    let name = '';
+    let display;
     useEffect(() => {
-        if (!userData.user)
+        if (!userData.user) {
             history.push('/login');
+            return;
+        }
+
+
     });
 
+    if (userData.user)
+        name = userData.user.display;
+
+
     return (
-        <div>
+        <main className={mainContent}>
             <h2>Home Page</h2>
-        </div>
+            <h4> Welcome, {name}</h4>
+        </main>
     )
 }
